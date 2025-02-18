358763bd598947446b5126bae97a3441bb4c149a
ogvieira <osmargv100@gmail.com>
2025-02-18 17:02:59 -0300

    break changes

--- src/app/app.component.cy.ts
+++ src/app/app.component.cy.ts
@@ -7,6 +7,6 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   it('deve renderizar o título corretamente', () => {
     mount(AppComponent); // Monta o componente
-    cy.get('h1').should('have.text', 'Hello, front-end'); // Verifica o texto do título
+    cy.get('h1').should('have.text', 'Hello,'); // Verifica o texto do título
   });
 });
